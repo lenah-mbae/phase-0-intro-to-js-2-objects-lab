5ab22e96dc12dcc986e137adf64751393af8fcdc
lenahmbae <lenahmbae22@gmail.com>
2025-03-25 10:01:57 +0300

    object management

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Sam',
+    StreetAddress: 'i Main St'
+};
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+        return {
+        ...employeeObj,
+        [key]: value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value) {
+      employeeObj[key] = value;
+    return employeeObj;
+}
+
+function deleteFromEmployeeByKey(employeeObj, key) {
+    
+    const newObj = { ...employeeObj };
+    delete newObj[key];
+    return newObj;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employeeObj, key) {
+    
+    delete employeeObj[key];
+    return employeeObj;
+}
